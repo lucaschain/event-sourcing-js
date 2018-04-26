b72782727f108101c76e7c15f7cf4b95ce1dbef4
Lucas Chain <sou@lucaschain.com.br>
2018-04-26 13:43:50 -0300

    using Array.prototype.push interface

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const state = require('./state');
 
 const eventList = [
   open('chain', 200),
-  open('eltinho', 100),
-  transfer('chain', 'eltinho', 50),
-  transfer('chain', 'eltinho', 30),
-  open('chain', 100000) //bug abuse
+  open('eltinho', 100)
 ];
+
+eventList.push(
+  transfer('chain', 'eltinho', 50)
+);
+
+eventList.push(
+  transfer('chain', 'eltinho', 30)
+);
+
+eventList.push(
+  open('chain', 100000) //triggers warning if trying to open existing account
+);
 
 console.log(state(eventList)); // { chain: 120, eltinho: 180 }
